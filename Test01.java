50f860438171e51c38ba0cd6561bb63e5f2bea1e
mazhuang <874416555@qq.com>
2016-11-07 09:27:48 +0800

    test01.java

---
+++ Test01.java
@@ -0,0 +1,23 @@
+package day04.ex;
+
+//1.写一个方法，接受一个整数，返回这个整数是几位数
+public class Test01 {
+	
+	// n代表输入的整数，返回值代表位数
+	static int count(int n) {
+		// 123
+		int m = n;
+		int c = 0;
+		while(m > 0) {
+			m = m / 10; // 0
+			c++;
+		}
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		int d = count(422222);
+		System.out.println("位数共:" + d);
+	}
+
+}
